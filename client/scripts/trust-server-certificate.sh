f15074725ef96f71920105183140cf8a4fe55213
Luiz Lelis <luizhlelis@gmail.com>
2020-12-13 12:31:13 -0300

    fix: unecessary sudo remotion

--- client/scripts/trust-server-certificate.sh
+++ client/scripts/trust-server-certificate.sh
@@ -5,4 +5,4 @@ apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
 #   The server cert is already in the folder /usr/local/share/ca-certificates/ (as cofigured in volume).
 #   The command below will make the client container to trust in server cert
 echo "Trusting server self signed cert"
-sudo update-ca-certificates
+update-ca-certificates
