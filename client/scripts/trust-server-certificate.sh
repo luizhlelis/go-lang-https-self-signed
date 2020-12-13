80da8671137a7772dc00d3d4f23784522b5b7790
Luiz Lelis <luizhlelis@gmail.com>
2020-12-13 13:36:00 -0300

    fix: wait for server to create the certificate before client trust

--- client/scripts/trust-server-certificate.sh
+++ client/scripts/trust-server-certificate.sh
@@ -2,7 +2,10 @@
 #   Installing ca-certificates package which comes with preinstalled certs and enables update with your third party certs
 echo "Installing ca-certificates package"
 apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
-#   The server cert is already in the folder /usr/local/share/ca-certificates/ (as cofigured in volume).
+echo "Waiting for the server to server create the certificate"
+sleep 20
+echo "Copy certificate to ca-certificates folder"
+cp ../certificates/servercert.crt /usr/local/share/ca-certificates/servercert.crt
 #   The command below will make the client container to trust in server cert
 echo "Trusting server self signed cert"
 update-ca-certificates
