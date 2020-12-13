47f1d4d731f6783fd15288a5a1b50cc2e9f7e0a4
Luiz Lelis <luizhlelis@gmail.com>
2020-12-13 13:35:13 -0300

    feature: curl installation and sleep time update

--- client/get-server-home.sh
+++ client/get-server-home.sh
@@ -1,4 +1,7 @@
-sleep 1m
-curl https://https-server/home
+echo "Installing curl package"
+apk update && apk add curl && rm -rf /var/cache/apk/*
+echo "Two requests below to get https server home"
 sleep 10
-curl https://https-server/home
+curl https://https-server:8081/home
+sleep 20
+curl https://https-server:8081/home
